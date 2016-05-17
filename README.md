5381999a9ccf963a02cc15e4538b80ac6f67888a
Mosè Giordano <mose@gnu.org>
2016-05-17 10:56:02 +0200

    Start coding

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # Measurements
 
 [![Build Status](https://travis-ci.org/giordano/Measurements.jl.svg?branch=master)](https://travis-ci.org/giordano/Measurements.jl)
+
+Introduction
+------------
+
+This package defines a new data type, `Measurement`, that allows you to enter a
+quantity with its uncertainty and propagate errors when performing mathematical
+operations involving `Measurement` objects.
+
+Examples
+--------
+
+``` julia
+using Measurements
+a = Measurement(4.5, 0.1)
+# => 4.5 ± 0.1
+b = Measurement(3.8, 0.4)
+# => 3.8 ± 0.4
+2a + b
+# => 12.8 ± 0.4472135954999579
+a - 1.2b
+# => -0.05999999999999961 ± 0.49030602688525043
+l = Measurement(0.936, 1e-3);
+T = Measurement(1.942, 4e-3);
+P = 4pi^2*l/T^2
+# => 9.797993213510699 ± 0.041697817535336676
+c = Constant(4)
+# => 4 ± 0
+a*c
+# => 18.0 ± 0.4
+sind(Measurement(94, 1.2))
+# => 0.9975640502598242 ± 0.0014609761696991563
+```
--- src/Measurements.jl
+++ src/Measurements.jl
@@ -1,5 +1,116 @@
+# This file is a part of Measurements.jl.  License is MIT "Expat".
+# Copyright (C) 2016 Mosè Giordano.
+
 module Measurements
 
-# package code goes here
+# Function(s) to handle new type
+import Base: show
+# Mathematical operations to be redefined
+import Base: +, -, *, /, inv, ^, cos, sin, deg2rad, rad2deg, cosd, sind, exp,
+             log, hypot, zero
+
+export Measurement, Constant
+
+# Define the new type
+type Measurement
+    value
+    err
+end
+# Constructors
+Constant(value) = Measurement(value, zero(value))
+Measurement(value) = Constant(value)
+
+# Type representation
+function show(io::IO, measure::Measurement)
+    print(measure.value, " ± ", measure.err)
+end
+
+##### Mathematical Operations
+# Addition: +
++(a::Measurement) = a
++(a::Measurement, b::Measurement) =
+    Measurement(promote(a.value + b.value, hypot(a.err, b.err))...)
++(a, b::Measurement) = +(Constant(a), b)
++(a::Measurement, b) = +(a, Constant(b))
+
+# Subtraction: -
+-(a::Measurement) = Measurement(-a.value, a.err)
+-(a::Measurement, b::Measurement) = a + (-b)
+-(a, b::Measurement) = -(Constant(a), b)
+-(a::Measurement, b) = -(a, Constant(b))
+
+# Multiplication: *
+function *(a::Measurement, b::Measurement)
+    prod = a.value*b.value
+    return Measurement(promote(prod, abs(prod)*hypot(a.err*inv(a.value),
+                                                     b.err*inv(b.value)))...)
+end
+*(a, b::Measurement) = *(Constant(a), b)
+*(a::Measurement, b) = *(a, Constant(b))
+
+# Division: /
+function /(a::Measurement, b::Measurement)
+    div = a.value*inv(b.value)
+    return Measurement(promote(div, abs(div)*(hypot(a.err*inv(a.value),
+                                                    b.err*inv(b.value))))...)
+end
+/(a, b::Measurement) = /(Constant(a), b)
+/(a::Measurement, b) = /(a, Constant(b))
+
+# Inverse: inv
+function inv(a::Measurement)
+    inverse = inv(a.value)
+    return Measurement(promote(inverse, inverse*inverse*a.err)...)
+end
+
+# Power: ^.  TODO: handle special cases (exponent = -1, etc...)
+function ^(a::Measurement, b::Measurement)
+    pow = a.value^b.value
+    return Measurement(promote(pow, hypot(pow*inv(a.value)*b.value*a.err,
+                                          pow*log(a.value)*b.err))...)
+end
+^{T<:Integer}(a::Measurement, b::T) = ^(a, Constant(b))
+^{T<:Number}(a::Measurement,  b::T) = ^(a, Constant(b))
+^{T<:Integer}(a::T, b::Measurement) = ^(Constant(a), b)
+^{T<:Number}(a::T,  b::Measurement) = ^(Constant(a), b)
+
+# deg2rad and rad2deg
+rad2deg(z::Measurement) = z*(180.0/pi)
+deg2rad(z::Measurement) = z*(pi/180.0)
+
+# Cosine: cos
+function cos(a::Measurement)
+    return Measurement(promote(cos(a.value), abs(sin(a.value)*a.err))...)
+end
+cosd(a::Measurement) = cos(deg2rad(a))
+
+# Sine: sin
+function sin(a::Measurement)
+    return Measurement(promote(sin(a.value),
+                               abs(cos(a.value)*a.err))...)
+end
+sind(a::Measurement) = sin(deg2rad(a))
+
+# Exponential: exp
+function exp(a::Measurement)
+    val = exp(a.value)
+    return Measurement(promote(val, abs(val*a.err))...)
+end
+
+# Logarithm: log
+log(a::Measurement) =
+    Measurement(promote(log(a.value), a.err*inv(a.value))...)
+
+# Hypotenuse: hypot
+function hypot(a::Measurement, b::Measurement)
+    val = hypot(a.value, b.value)
+    return Measurement(promote(val, abs(hypot(a.value*a.err,
+                                              b.value*b.err)*inv(val)))...)
+end
+hypot(a, b::Measurement) = hypot(Constant(a), b)
+hypot(a::Measurement, b) = hypot(a, Constant(b))
+
+# Zero: zero
+zero(a::Measurement) = Measurement(promote(zero(a.value), zero(a.err))...)
 
 end # module
