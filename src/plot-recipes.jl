b1f698787c7af35db97b71009b88c86d6db71f69
Mosè Giordano <mose@gnu.org>
2018-07-17 00:30:20 +0200

    Change Vector → Array in plot recipes

--- src/plot-recipes.jl
+++ src/plot-recipes.jl
@@ -17,30 +17,30 @@
 
 using RecipesBase
 
-@recipe function f(y::AbstractVector{<:Measurement})
+@recipe function f(y::AbstractArray{<:Measurement})
     yerror := uncertainty.(y)
     value.(y)
 end
 
-@recipe function f(func::Function, x::AbstractVector{<:Measurement})
+@recipe function f(func::Function, x::AbstractArray{<:Measurement})
     y = func.(x)
     xerror := uncertainty.(x)
     yerror := uncertainty.(y)
     value.(x), value.(y)
 end
 
-@recipe function f(x::AbstractVector{<:Measurement}, y::AbstractVector{<:Measurement})
+@recipe function f(x::AbstractArray{<:Measurement}, y::AbstractArray{<:Measurement})
     xerror := uncertainty.(x)
     yerror := uncertainty.(y)
     value.(x), value.(y)
 end
 
-@recipe function f(x::AbstractVector{<:Measurement}, y::AbstractVector)
+@recipe function f(x::AbstractArray{<:Measurement}, y::AbstractArray)
     xerror := uncertainty.(x)
     value.(x), y
 end
 
-@recipe function f(x::AbstractVector, y::AbstractVector{<:Measurement})
+@recipe function f(x::AbstractArray, y::AbstractArray{<:Measurement})
     yerror := uncertainty.(y)
     x, value.(y)
 end
