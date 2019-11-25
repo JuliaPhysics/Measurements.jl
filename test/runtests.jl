86339f903178ee55b012fa377bba2851e45eb66c
simeonschaub <simeondavidschaub99@gmail.com>
2019-11-25 23:59:47 +0100

    fix ranges of Measurements without `stop` argument (#56)
    * fix ranges of Measurements without `stop` argument
    
    * add tests from #55

--- test/runtests.jl
+++ test/runtests.jl
@@ -783,3 +783,14 @@ end
 @testset "Plot recipes" begin
     include("plots.jl")
 end
+
+@testset "Ranges" begin
+    m = 2 ± 1
+    @test m .+ (1:6) isa StepRangeLen
+    @test m .* (1:2:6) isa StepRangeLen
+    @test (1:.1:6) .- m isa StepRangeLen
+    @test m ./ (1:6.0) isa Vector{<:Measurement}
+    
+    # JuliaLang/julia#30944
+    @test range(0±0, step=1±.1, length=10) isa StepRangeLen
+end
