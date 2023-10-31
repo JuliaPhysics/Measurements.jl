ad9f0fca49b74583632811a722b553d61fc93658
Andrés Riedemann <38795484+longemen3000@users.noreply.github.com>
2023-10-31 08:40:31 -0300

    Fix MeasurementsJunoExt.jl (#159)

--- ext/MeasurementsJunoExt.jl
+++ ext/MeasurementsJunoExt.jl
@@ -29,8 +29,6 @@ end
 Juno.render(i::Juno.Inline, measure::Measurement) =
 Juno.render(i, Juno.Row(measure.val, Text(" ± "), measure.err))
 
-Juno.Row(measure.val, Text(" ± "), measure.err)
-
 function Juno.render(ji::Juno.Inline, cm::Complex{<:Measurement})
     r, i = reim(cm)
     if signbit(i) && !isnan(i)
