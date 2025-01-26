387501fdf1d926c3deeabf07d6e0519e4b9868fe
Valentin Churavy <v.churavy@gmail.com>
2025-01-26 15:05:17 +0100

    fixup! add test

--- test/makie.jl
+++ test/makie.jl
@@ -1,4 +1,13 @@
-import Makie: convert_arguments, Point
+import Makie: convert_arguments, Point, PointBased, Errorbars, Band
 @testset "PointBased" begin
     @test convert_arguments(PointBased(), [(1, 2)], [(Measurements.uncertainty(1.0, 0.5), Measurements.uncertainty(2.0, 0.1))]) == (Point{2,Float64}[[1.0, 2.0], [1.0, 2.0]],)
+    @test convert_arguments(PointBased(), [(Measurements.uncertainty(1.0, 0.5), Measurements.uncertainty(2.0, 0.1))], [(1, 2)]) == (Point{2,Float64}[[1.0, 2.0], [1.0, 2.0]],)
+    @test convert_arguments(PointBased(), [(Measurements.uncertainty(1.0, 0.5), Measurements.uncertainty(2.0, 0.1))],
+                                          [(Measurements.uncertainty(1.0, 0.5), Measurements.uncertainty(2.0, 0.1))]) == (Point{2,Float64}[[1.0, 2.0], [1.0, 2.0]],)
+end
+
+@testset "Errorbars" begin
+end
+
+@testset "Band" begin
 end
